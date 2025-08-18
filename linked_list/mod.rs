bebcf324ccdfa551005d36183fa66416ccd0ba3d
bagger3025 <bagger3025@g.skku.edu>
2025-08-18 13:18:54 +0900

    linkd list

---
+++ linked_list/mod.rs
@@ -0,0 +1,323 @@
+use std::{fmt::Debug, marker::PhantomData, mem::ManuallyDrop, ptr::null_mut};
+
+#[derive(Debug)]
+pub struct LinkedList<T> {
+    head: *mut LinkedListNode<T>,
+    tail: *mut LinkedListNode<T>,
+    len: usize,
+}
+
+struct LinkedListNode<T> {
+    data: T,
+    next: *mut LinkedListNode<T>,
+    prev: *mut LinkedListNode<T>,
+}
+
+impl<T> LinkedListNode<T> {
+    fn new(data: T) -> Self {
+        Self {
+            data,
+            next: null_mut(),
+            prev: null_mut(),
+        }
+    }
+
+    fn new_ptr(data: T) -> *mut Self {
+        let node = Box::new(Self::new(data));
+        Box::into_raw(node)
+    }
+}
+
+trait DropPointer {
+    fn drop(self);
+}
+
+trait MoveForward {
+    fn move_forward(&mut self);
+}
+
+trait MoveBackward {
+    fn move_backward(&mut self);
+}
+
+trait GetData<T> {
+    fn get_data(&self) -> Option<&T>;
+}
+
+impl<T> DropPointer for *const LinkedListNode<T> {
+    fn drop(self) {
+        drop(unsafe { Box::from_raw(self.cast_mut()) });
+    }
+}
+
+impl<T> MoveForward for *mut LinkedListNode<T> {
+    fn move_forward(&mut self) {
+        if self.is_null() {
+            return;
+        }
+
+        let node = *self;
+        *self = unsafe { (*node).next };
+        node.drop();
+
+        if !self.is_null() {
+            let data = *self;
+            unsafe {
+                (*data).prev = null_mut();
+            }
+        }
+    }
+}
+
+impl<T> MoveBackward for *mut LinkedListNode<T> {
+    fn move_backward(&mut self) {
+        if self.is_null() {
+            return;
+        }
+
+        let node = *self;
+        *self = unsafe { (*node).prev };
+        node.drop();
+        if !self.is_null() {
+            let data = *self;
+            unsafe {
+                (*data).next = null_mut();
+            }
+        }
+    }
+}
+
+impl<T> GetData<T> for *mut LinkedListNode<T> {
+    fn get_data(&self) -> Option<&T> {
+        match (*self).is_null() {
+            true => None,
+            false => Some(unsafe { &(**self).data }),
+        }
+    }
+}
+
+impl<T> GetData<T> for *const LinkedListNode<T> {
+    fn get_data(&self) -> Option<&T> {
+        match (*self).is_null() {
+            true => None,
+            false => Some(unsafe { &(**self).data }),
+        }
+    }
+}
+
+impl<T> LinkedList<T> {
+    pub fn new() -> Self {
+        LinkedList {
+            head: null_mut(),
+            tail: null_mut(),
+            len: 0,
+        }
+    }
+
+    fn push_first(&mut self, data: T) {
+        assert!(self.head.is_null());
+        assert!(self.tail.is_null());
+
+        let node = LinkedListNode::new_ptr(data);
+        self.head = node;
+        self.tail = node;
+    }
+
+    pub fn push_back(&mut self, data: T) {
+        if self.head.is_null() {
+            assert!(self.len == 0);
+            self.push_first(data);
+        } else {
+            assert!(self.len > 0);
+            let tail = self.tail;
+            let new_node = LinkedListNode::new_ptr(data);
+            unsafe {
+                (*new_node).prev = tail;
+                (*tail).next = new_node;
+            }
+            self.tail = new_node;
+        }
+        self.len += 1;
+    }
+
+    pub fn push_front(&mut self, data: T) {
+        if self.head.is_null() {
+            assert!(self.len == 0);
+            self.push_first(data);
+        } else {
+            assert!(self.len != 0);
+            let head = self.head;
+            let new_node = LinkedListNode::new_ptr(data);
+            unsafe {
+                (*new_node).next = head;
+                (*head).prev = new_node;
+            }
+            self.head = new_node;
+        }
+        self.len += 1;
+    }
+
+    pub fn front(&self) -> Option<&T> {
+        self.head.get_data()
+    }
+
+    pub fn pop_front(&mut self) {
+        self.head.move_forward();
+
+        self.len -= 1;
+        if self.head.is_null() {
+            self.tail = null_mut();
+        }
+    }
+
+    pub fn back(&self) -> Option<&T> {
+        self.tail.get_data()
+    }
+
+    pub fn pop_back(&mut self) {
+        self.tail.move_backward();
+
+        self.len -= 1;
+        if self.tail.is_null() {
+            self.head = null_mut();
+        }
+    }
+
+    pub fn print(&self)
+    where
+        T: Debug,
+    {
+        let mut head = self.head;
+        while !head.is_null() {
+            unsafe {
+                print!("{:?} ", (*head).data);
+                head = (*head).next;
+            }
+        }
+        println!();
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
+    }
+}
+
+impl<T> Drop for LinkedList<T> {
+    fn drop(&mut self) {
+        println!("LinkedList is dropped!");
+        let mut head = self.head;
+        while !head.is_null() {
+            let cur_head = head;
+            head = unsafe { (*head).next };
+
+            drop(unsafe { Box::from_raw(cur_head) });
+        }
+        self.head = null_mut();
+        self.tail = null_mut();
+        self.len = 0;
+    }
+}
+
+pub struct LinkedListIntoIterator<T> {
+    node: *const LinkedListNode<T>,
+    _unused: PhantomData<LinkedList<T>>,
+    _unused_self: ManuallyDrop<LinkedList<T>>,
+}
+
+pub struct LinkedListIterator<'a, T> {
+    node: Option<&'a LinkedListNode<T>>,
+    _unused: PhantomData<&'a LinkedList<T>>,
+}
+
+impl<'a, T> Iterator for LinkedListIterator<'a, T>
+where
+    T: 'a,
+{
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.node {
+            Some(node) => {
+                self.node = unsafe { node.next.as_ref() };
+                Some(&node.data)
+            }
+            None => None,
+        }
+    }
+}
+
+impl<'a, T> IntoIterator for &'a LinkedList<T> {
+    type Item = &'a T;
+    type IntoIter = LinkedListIterator<'a, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        Self::IntoIter {
+            node: if self.head.is_null() {
+                None
+            } else {
+                unsafe { self.head.as_ref() }
+            },
+            _unused: PhantomData,
+        }
+    }
+}
+
+impl<T> IntoIterator for LinkedList<T> {
+    type Item = T;
+    type IntoIter = LinkedListIntoIterator<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        let LinkedList { head, .. } = self;
+
+        let ret = LinkedListIntoIterator {
+            node: head,
+            _unused: PhantomData,
+            _unused_self: ManuallyDrop::new(self),
+        };
+        // self.head = null_mut();
+        // self.tail = null_mut();
+        // self.len = 0;
+        ret
+    }
+}
+
+trait GetDataInto<T> {
+    fn get_data_into(self) -> T;
+}
+
+impl<T> GetDataInto<T> for Box<LinkedListNode<T>> {
+    fn get_data_into(self) -> T {
+        self.data
+    }
+}
+
+impl<T> Iterator for LinkedListIntoIterator<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.node.is_null() {
+            None
+        } else {
+            let node = self.node;
+            let node = unsafe { Box::from_raw(node.cast_mut()) };
+
+            self.node = node.next;
+
+            let data = node.get_data_into();
+
+            Some(data)
+        }
+    }
+}
+
+impl<T> Drop for LinkedListIntoIterator<T> {
+    fn drop(&mut self) {
+        println!("LinkedList Into Iterator dropped!");
+        let mut head = self.node;
+        while !head.is_null() {
+            let node = head;
+            head = unsafe { (*head).next };
+            drop(unsafe { Box::from_raw(node.cast_mut()) });
+        }
+    }
+}
---
+++ main.rs
@@ -0,0 +1,173 @@
+use std::{fmt::Debug, marker::PhantomData};
+
+use linked_list::LinkedList;
+
+mod linked_list;
+// use std::{borrow::Borrow, cell::RefCell, rc::Rc};
+
+// struct LinkedList {
+//     head: Option<Rc<RefCell<Box<LinkedListNode>>>>,
+//     tail: Option<Rc<RefCell<Box<LinkedListNode>>>>,
+// }
+
+// struct LinkedListNode {
+//     data: i32,
+//     next: Option<Rc<RefCell<Box<LinkedListNode>>>>,
+// }
+
+// impl LinkedListNode {
+//     fn new(val: i32) -> LinkedListNode {
+//         LinkedListNode {
+//             data: val,
+//             next: None,
+//         }
+//     }
+// }
+
+// impl LinkedList {
+//     fn new() -> LinkedList {
+//         LinkedList {
+//             head: None,
+//             tail: None,
+//         }
+//     }
+
+//     fn insert(&mut self, val: i32) {
+//         match &self.tail {
+//             Some(tail) => {
+//                 let node = Rc::new(RefCell::new(Box::new(LinkedListNode::new(val))));
+//                 tail.clone().as_ref().borrow_mut().next = Some(Rc::clone(&node));
+//                 self.tail = Some(Rc::clone(&node));
+//             }
+//             None => {
+//                 let node = Rc::new(RefCell::new(Box::new(LinkedListNode::new(val))));
+
+//                 self.head = Some(Rc::clone(&node));
+//                 self.tail = Some(Rc::clone(&node));
+//             }
+//         }
+//     }
+
+//     fn delete(&mut self, val: i32) {
+//         if self.head.is_none() {
+//             return;
+//         }
+
+//         let mut node = self.head.clone();
+
+//         if self.head.as_ref().unwrap().as_ref().borrow().data == val {
+//             let next_node = self.head.as_ref().unwrap().as_ref().borrow().next.clone();
+//             self.head = next_node;
+//             if self.head.is_none() {
+//                 self.tail = None;
+//             }
+//         }
+
+//         while node.is_some() {
+//             let next_option = node.as_ref().unwrap().as_ref().borrow().next.clone();
+//             let next_node = next_option.as_ref().unwrap().as_ref().borrow();
+//             println!("{:?}, {:?}", &next_node.data, &val);
+//             if next_node.data == val {
+//                 let next_linked_node = next_node.next.clone();
+//                 drop(next_node);
+//                 node.as_ref().unwrap().as_ref().borrow_mut().next = next_linked_node.clone();
+//                 if next_linked_node.is_none() {
+//                     self.tail = node;
+//                 }
+//                 return;
+//             }
+//             drop(next_node);
+//             node = next_option;
+//         }
+//     }
+
+//     fn print(&self) {
+//         let mut rc;
+//         let mut node = self.head.clone();
+
+//         while node.is_some() {
+//             print!("{:?} ", &node.as_ref().unwrap().as_ref().borrow().data);
+//             rc = node.as_ref().unwrap().clone();
+//             node = rc.as_ref().borrow().next.clone();
+//         }
+//         println!();
+//     }
+// }
+
+#[derive(Debug)]
+struct MyStruct<T> {
+    data: i32,
+    _unused: PhantomData<T>,
+}
+
+impl<T> MyStruct<T> {
+    fn push(&mut self, data: T)
+    where
+        T: Debug,
+    {
+        println!("{data:?}");
+    }
+}
+
+fn main() {
+    let s = String::from("s");
+    let mut vec = vec![];
+
+    let mut ms = MyStruct {
+        data: 3,
+        _unused: PhantomData,
+    };
+
+    {
+        let t = String::from("t");
+
+        println!("{:?}", vec);
+
+        vec.push(&t);
+        vec.push(&s);
+
+        println!("{:?}", vec);
+
+        // drop(t);
+        // println!("{:?}", vec);
+
+        println!("{:?}", ms);
+        ms.push(&t);
+        drop(t);
+        // println!("{:?}", ms);
+    }
+
+    let mut head = LinkedList::<i32>::new();
+
+    head.push_back(3);
+    head.push_front(4);
+    head.push_back(5);
+
+    head.print();
+    println!("");
+
+    println!(
+        "Front is {}, back is {}",
+        head.front().unwrap(),
+        head.back().unwrap()
+    );
+    println!("{}", head.len());
+
+    // print!("pop front => ");
+    // head.pop_front();
+    // head.print();
+
+    // print!("pop back => ");
+    // head.pop_back();
+    // head.print();
+
+    for data in &head {
+        println!("data = {data}");
+    }
+
+    for data in head {
+        println!("data = {data}");
+    }
+
+    println!("End of main")
+}
